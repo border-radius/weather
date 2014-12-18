2b8cefc731afcbf19c43ce3e6e4611b15a32a948
border-radius <pharshmaster@gmail.com>
2014-12-18 23:57:08 +0300

    IATA test update

--- test/iata.js
+++ test/iata.js
@@ -1,11 +1,15 @@
 var should = require('should');
 var iata = require('../iata');
 
-var VKO = iata('VKO');
+describe('IATA', function () {
+  it('should return valid coordinates', function () {
+    var VKO = iata('VKO');
 
-(VKO === undefined).should.be.false;
+    (VKO === undefined).should.be.false;
 
-VKO.should.be.an.instanceOf(Array);
-VKO.should.have.length(2);
-VKO[0].should.be.approximately(55.5913, 0.001);
-VKO[1].should.be.approximately(37.2613, 0.001);
+    VKO.should.be.an.instanceOf(Array);
+    VKO.should.have.length(2);
+    VKO[0].should.be.approximately(55.5913, 0.001);
+    VKO[1].should.be.approximately(37.2613, 0.001);
+  });
+});
