2905f4d9c218f2eda6145c5aebdc96e0e7bc9279
border-radius <pharshmaster@gmail.com>
2014-12-19 11:22:25 +0300

    Wunderground test

--- test/weather.js
+++ test/weather.js
@@ -1,5 +1,6 @@
 var should = require('should');
 var owm = require('../weather/openweathermap');
+var wunderground = require('../weather/wunderground');
 
 describe('OpenWeatherMap', function () {
   it('should return valid historical data', function () {
@@ -9,5 +10,14 @@ describe('OpenWeatherMap', function () {
       temp.should.be.approximately(-6.77, 0.01);
     });
 
+  });
+});
+
+describe('Wunderground', function () {
+  it('should return valid historical data', function () {
+    wunderground([55.5913, 37.2613], 1416528000, function (e, temp) {
+      e.should.be.not.ok;
+      temp.should.be.approximately(-6.77, 0.01);
+    });
   });
 });
