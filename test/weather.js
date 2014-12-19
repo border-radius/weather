0177a07e017cc67fe4ea4a5138ef6073cdce0089
border-radius <pharshmaster@gmail.com>
2014-12-19 12:13:28 +0300

    Wunderground test fix

--- test/weather.js
+++ test/weather.js
@@ -17,7 +17,7 @@ describe('Wunderground', function () {
   it('should return valid historical data', function () {
     wunderground([55.5913, 37.2613], 1416528000, function (e, temp) {
       e.should.be.not.ok;
-      temp.should.be.approximately(-6.77, 0.01);
+      temp.should.be.equal(-7);
     });
   });
 });
